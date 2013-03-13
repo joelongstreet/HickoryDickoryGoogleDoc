da98840180a925af70eca6471f6e2f35fbcb4eb3
joelongstreet <joelongstreet@gmail.com>
2013-03-13 14:11:51 -0500

    updating script for better env vars

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ var fs          = require('fs');
 var csv         = require('csv-string');
 var request     = require('request');
 
-var earl        = process.env.GOOGLE_DOC_PATH;
-var filePath    = 'output.json';
-if(process.env.OUTPUT_PATH) filePath = process.env.OUTPUT_PATH;
+if(process.env.GDOC_PATH == undefined){
+    console.log('Please specify a google doc path, see the readme for directions.');
+    process.exit()
+}
 
-var keyIndex    = 1;
+var earl        = process.env.GDOC_PATH;
+var filePath    = 'output.json';
+var keyIndex    = 0;
+if(process.env.OUTPUT_PATH) filePath = process.env.OUTPUT_PATH;
+if(process.env.START_INDEX) keyIndex = process.env.START_INDEX;
+
 var jsonReturn  = []
 var keys        = [];
 
@@ -25,6 +31,6 @@ request(earl, function(err, response, body){
 
     fs.writeFile(filePath, JSON.stringify(jsonReturn, null, 4), 'utf8', function(err){
         if(err) console.log('err');
-        else console.log('Done writing file at ' + filePath);
+        else console.log('\n\nDone writing json file, located at ' + filePath + '.\n\nObject keys are : \n' + JSON.stringify(keys, null, 4));
     });
 });
