7e6c7a990ca6350ad6b581fe280b15052ff8d19d
joelongstreet <joelongstreet@gmail.com>
2013-04-02 18:36:06 -0500

    just a bunch more stuff ya know

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs          = require('fs');
 var csv         = require('csv-string');
+var prompt      = require('prompt');
 var request     = require('request');
 var color       = require('cli-color');
 
@@ -8,7 +9,7 @@ var printHelp = function(){
     console.log('\nUsage: hickorydickory [options]');
     console.log('\nOptions:');
     console.log('  -k, --key            set the value of the google doc key ot be used');
-    console.log('  -r, --row            set the row to be used as the object key');
+    console.log('  -i, --index          set the row to be used as the object key index');
     console.log('  -h, --help           see help');
     console.log('\nDocumentation at https://github.com/joelongstreet/HickoryDickoryGoogleDoc');
 };
@@ -16,21 +17,48 @@ var printHelp = function(){
 var printError = function(err){
     console.log(color.red.bold(err));
     process.exit();
-}
+};
 
-var executeScript = function(options){
-    if(!(options.gdocKey)){ printError('Please specify a google doc path, see the readme for directions.') };
+var makePrompt = function(){
+    prompt.start();
+    prompt.get({
+        properties : {
+            key         : { description : 'What\'s the Google Doc Key for the file you\'d like to use? See the README for more information on how to obtain this...'},
+            index       : { description : 'At what index (0 based) should the object keys be defined?'},
+            file        : { description : 'Where should I store the outputted file (example - /assets/data.json)?'}
+            //exports     : { description : 'Surround result in module.exports (true of false)?'}
+        }
+    }, function(err, result){
+        if(err) { printError(err); }
+        else {
+            writeConfig(result, function(){
+                console.log('Thanks, a new configuration file has been created at ' + process.cwd() + '/.hickorydickory');
+                console.log('If you need to make changes, that\'s where you\'ll do it');
+                // would be great to add to .gitignore here
+                execute(result);
+            });
+        }
+    });
+};
 
-    var earl        = 'https://docs.google.com/spreadsheet/pub?key=' + options.gdocKey + '&output=csv';
-    var fileName    = 'data.json';
-    var filePath    = process.cwd() + '/';
-    var keyIndex    = 0;
+var readConfig = function(){
+    fs.readFile('.hickorydickory', 'utf-8', function(err, data){
+        if(err) { makePrompt();  }
+        else    { execute(JSON.parse(data)); }
+    });
+};
 
-    if(process.env.FILE_NAME)   fileName = process.env.FILE_NAME;
-    if(process.env.FILE_PATH)   filePath = process.env.FILE_PATH;
-    if(process.env.START_INDEX) keyIndex = process.env.START_INDEX;
+var writeConfig = function(data, next){
+    fs.writeFile('.hickorydickory', JSON.stringify(data, null, 4), 'utf8', function(err){
+        if(err)  { printError(err); }
+        if(next) { next(); }
+    });
+};
 
-    var jsonReturn  = []
+var execute = function(options){
+
+    var earl        = 'https://docs.google.com/spreadsheet/pub?key=' + options.key + '&output=csv';
+    var jsonReturn  = [];
     var keys        = [];
 
     request.get(earl, function(err, response, body){
@@ -38,36 +66,27 @@ var executeScript = function(options){
         if(err){ printError(err); }
 
         var rows = csv.parse(body);
-
         for(var i=0; i<rows.length; i++){
             var obj = {}
             for(var j=0; j<rows[i].length; j++){
-                if(i == keyIndex) keys.push(rows[i][j]);
-                else obj[keys[j]] = rows[i][j];
+                if(i == options.index) keys.push(rows[i][j]);
+                else
+                    if(rows[i][j].indexOf('[') == 0) {
+                        var content = rows[i][j].replace('[', '').replace(']', '').split(',');
+                        obj[keys[j]] = content;
+                    } else{
+                        obj[keys[j]] = rows[i][j];
+                    }
             }
 
-            if(i > keyIndex) jsonReturn.push(obj);
+            if(i > options.index) jsonReturn.push(obj);
         }
 
-        fs.writeFile(filePath + fileName, JSON.stringify(jsonReturn, null, 4), 'utf8', function(err){
+        fs.writeFile(options.file, JSON.stringify(jsonReturn, null, 4), 'utf8', function(err){
             if(err) console.log('err');
-            else console.log('\n\nDone writing json file, located at ' + filePath + fileName + '.\n\nObject keys are : \n' + JSON.stringify(keys, null, 4));
+            else console.log('\n\nDone writing json file, located at ' + options.file + '.\n\nObject keys are : \n' + JSON.stringify(keys, null, 4));
         });
     });
 };
 
-
-var args = process.argv;
-if(args.indexOf('-h') != -1){
-    printHelp();
-} else if(args.indexOf('--help') != -1){
-    printHelp();
-} else if(args.indexOf('-k') != -1){
-    var index = args.indexOf('-k') + 1;
-    executeScript(args[index]);
-} else if(args.indexOf('--key') != -1){
-    var index = args.indexOf('--key') + 1;
-    executeScript(args[index]);
-} else {
-    executeScript();
-}
+readConfig();
